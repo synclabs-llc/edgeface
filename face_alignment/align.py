c8cb7b10cc9c10cc351ef7859f5d5c609e5c5782
Rajnish Gupta <rajnishg74@gmail.com>
2025-07-07 09:50:48 -0700

    restore face-alignment, for testign with embeddings

--- face_alignment/align.py
+++ face_alignment/align.py
@@ -1,13 +1,13 @@
 import sys
 import os
 
-from face_alignment import mtcnn_tf
+from face_alignment import mtcnn
 import argparse
 from PIL import Image
 from tqdm import tqdm
 import random
 from datetime import datetime
-mtcnn_model = mtcnn_tf.MTCNNTFAligner( crop_size=(112, 112))
+mtcnn_model = mtcnn.MTCNN(device='cuda:0', crop_size=(112, 112))
 
 def add_padding(pil_img, top, right, bottom, left, color=(0,0,0)):
     width, height = pil_img.size
@@ -33,19 +33,4 @@ def get_aligned_face(image_path, rgb_pil_image=None):
         face = None
 
     return face
-
-def get_aligned_face_from_image(rgb_pil_image):
-    assert isinstance(rgb_pil_image, Image.Image), 'Input must be a PIL image'
-    img = rgb_pil_image
-    try:
-        bboxes, faces = mtcnn_model.align_multi(img, limit=1)
-        face = faces[0]
-    except Exception as e:
-        print('Face detection Failed due to error.')
-        print(e)
-        face = None
-
-    return face
-
-
 
